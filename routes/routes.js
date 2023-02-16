2e3e7b5b758e61ffc6c7ac843fea420ab4755212
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-02-16 20:42:38 +0700

    Update Koneksi ke Database

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mysql from "mysql";
 
 import { Home, Lomba, Peserta } from "../controllers/home.js";
 
@@ -7,6 +8,14 @@ const router = express.Router();
 global.app = express();
 
 router.use(express.static('views'));
+
+global.connection = mysql.createConnection({
+    host : '5.181.216.42',
+    port : '3306',
+    user : 'u1084987_kelompok_5',
+    password : '8gwfyqbwgb',
+    database : 'u1084987_kelompok_5'
+});
 
 router.get('/', Home);
 router.get('/lomba', Lomba);
